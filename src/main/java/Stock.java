a71e1922042a6900f1d97492b3175a5687946bea
ruagegd <elena.kosheleva@raiffeisen.ru>
2021-07-25 17:31:30 +0300

    task 1

---
+++ src/main/java/Stock.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+
+    public static final int TIME_OF_SELLING = 500;
+
+    Showroom showroom;
+    List<Car> cars = new ArrayList<>();
+
+
+    public Stock(Showroom showroom) {
+        this.showroom = showroom;
+    }
+        public synchronized Car sellCar(){
+            try {
+                if (cars.isEmpty()) {
+                    System.out.printf("для клиента %s Нет авто в наличии! ждите\n",Thread.currentThread().getName());
+                    while (cars.isEmpty()) {
+                        wait();
+                    }
+                }
+                System.out.println("Автомобиль " + cars.get(0) + " продан ");
+                return cars.remove(0);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
+        public synchronized void receiveCar (Car car) {
+            try {
+                System.out.println("Приход Авто в австосалон");
+                Thread.sleep(TIME_OF_SELLING);
+                cars.add(car);
+                System.out.printf("на складе %s авто\n",cars.size());
+                notify();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+
+            }
+
+        }
+
+}
