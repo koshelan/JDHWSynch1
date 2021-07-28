4f3e688c4a6346926622f0ea04fc963343febe13
ruagegd <elena.kosheleva@raiffeisen.ru>
2021-07-28 22:07:48 +0300

    bugs fixed

--- src/main/java/Stock.java
+++ src/main/java/Stock.java
@@ -12,34 +12,34 @@ public class Stock {
     public Stock(Showroom showroom) {
         this.showroom = showroom;
     }
-        public synchronized Car sellCar(){
-            try {
-                if (cars.isEmpty()) {
-                    System.out.printf("для клиента %s Нет авто в наличии! ждите\n",Thread.currentThread().getName());
-                    while (cars.isEmpty()) {
-                        wait();
-                    }
+
+    public synchronized Car sellCar() {
+        try {
+            if (cars.isEmpty()) {
+                System.out.printf("для клиента %s Нет авто в наличии! ждите\n", Thread.currentThread().getName());
+                while (cars.isEmpty()) {
+                    wait();
                 }
-                System.out.println("Автомобиль " + cars.get(0) + " продан ");
-                return cars.remove(0);
-            } catch (Exception ex) {
-                ex.printStackTrace();
             }
-            return null;
+            System.out.println("Автомобиль " + cars.get(0) + " продан ");
+            return cars.remove(0);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public synchronized void receiveCar(Car car) {
+        try {
+            System.out.println("Приход Авто в австосалон");
+            cars.add(car);
+            System.out.printf("на складе %s авто\n", cars.size());
+            notify();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
         }
 
-        public synchronized void receiveCar (Car car) {
-            try {
-                System.out.println("Приход Авто в австосалон");
-                Thread.sleep(TIME_OF_SELLING);
-                cars.add(car);
-                System.out.printf("на складе %s авто\n",cars.size());
-                notify();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-
-            }
-
-        }
+    }
 
 }
